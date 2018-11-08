710e394f77bfc60a8a50a7f7bd384730ed2ce2e8
Ben Clark <benclark@udel.edu>
2018-11-08 15:56:23 -0500

    interactivce chart

--- coffee/graph.js
+++ coffee/graph.js
@@ -1,4 +1,25 @@
+function generateData(caffine) {
+    var data = [];
+    var limit = 48;
+    var y = 0;
+    var dataSeries = { type: "line" };
+    var dataPoints = [];
+    for (var i = 0; i < limit; i += 1) {
+        y = caffine*Math.pow(0.5, i/6)  // caffine half-life
+        dataPoints.push({
+                        x: i,
+                        y: y
+                        });
+    }
+    dataSeries.dataPoints = dataPoints;
+    data.push(dataSeries);
+    return data;
+}
+
 window.onload = function () {
+    
+    var data = generateData(120);
+    
     var chart = new CanvasJS.Chart("chartContainer",
     {
         title: {
@@ -10,23 +31,17 @@ window.onload = function () {
         axisY: {
             title: "Caffine (mm)"
         },
-        data: data  // generated below
+        data: data
     });
-
+    
     chart.render();
+    
+    function addDataPointsAndRender() {
+        caffine = Number(document.getElementById('caffine').value);
+        chart.options.data = generateData(caffine);
+        chart.render();
+    }
+    
+    var renderButton = document.getElementById('renderButton');
+    renderButton.addEventListener('click', addDataPointsAndRender);
 }
-
-var limit = 48;
-
-var y = 0;
-var data = []; var dataSeries = { type: "line" };
-var dataPoints = [];
-for (var i = 0; i < limit; i += 1) {
-    y = 120*Math.pow(0.5, i/6)  // caffine half-life
-    dataPoints.push({
-        x: i,
-        y: y
-    });
-}
-dataSeries.dataPoints = dataPoints;
-data.push(dataSeries);
