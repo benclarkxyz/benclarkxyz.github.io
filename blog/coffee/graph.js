0698168f23bc2ffbada977b38d7489a6c6e275b3
Ben Clark <benclark@udel.edu>
2018-11-21 16:19:21 -0500

    working on caffeine blog; additional dose coming soon

--- blog/coffee/graph.js
+++ blog/coffee/graph.js
@@ -1,24 +1,6 @@
-function generateData(caffeine) {
-    var data = [];
-    var limit = 48;
-    var y = 0;
-    var dataSeries = { type: "line" };
-    var dataPoints = [];
-    for (var i = 0; i < limit; i += 1) {
-        y = caffeine*Math.pow(0.5, i/5.7)  // caffeine half-life
-        dataPoints.push({
-                        x: i,
-                        y: y
-                        });
-    }
-    dataSeries.dataPoints = dataPoints;
-    data.push(dataSeries);
-    return data;
-}
-
 window.onload = function () {
-    var caffeine = 130;
-    var data = generateData(caffeine);
+    var caffeine = 130; // default caffeine amount
+    var data = createCurve(caffeine);
     
     var chart = new CanvasJS.Chart("chartContainer",
     {
@@ -30,36 +12,52 @@ window.onload = function () {
         },
         axisY: {
             title: "Caffeine (mg)",
-            stripLines: [{
-                value: caffeine/20,
-                color: "#ff0000",
-                label: "5%",
-//                labelPlacement: "outside"
-            }]
         },
         data: data
     });
     
-    chart.render();
+    chart.render(); // render default chart
     
-    function addDataPointsAndRender() {
-        caffeine = Number(document.getElementById('caffeine').value);
-        chart.options.data = generateData(caffeine);
-        chart.options.axisY.stripLines[0].value = caffeine/20;
-//        document.getElementById('fivePercent').innerHTML = caffeine   // 5% is ~25 hrs always
-        chart.render();
-    }
-    
+    // when user changes initial caffeine amount
     var renderButton = document.getElementById('renderButton');
-    renderButton.addEventListener('click', addDataPointsAndRender);
+    renderButton.addEventListener('click', function () {
+        caffeine = Number(document.getElementById('caffeine').value);
+        
+        chart.options.data = createCurve(caffeine);
+        
+        var t_2mg = 8.22336173*Math.log(caffeine) - 5.7   // half-life formula solved for t
+        document.getElementById('two_mg').innerHTML = t_2mg.toFixed(2) + " hours"
+        
+        chart.render();
+    });
     
-    var newCaffeineButton = document.getElementById('newCaffeineButton');
-    newCaffeineButton.addEventListener('click', function() {
+//    // when user adds additional dose
+//    var newCaffeineButton = document.getElementById('newCaffeineButton');
+//    newCaffeineButton.addEventListener('click', function() {
 //        var x = document.getElementsByTagName("div")[0];
 //
 //        if (x.id == "newCaffeineDose") {
 //            x.in
 //        }
-        document.getElementsByClassName('newCaffeineDose').innerHTML = "<h5 class='titleButton'>Additional caffeine dose in milligrams: <input id='newCaffeine' type='number' placeholder='Additional caffeine (mg) amount'></h5>"
-    });
+//    }
+}
+
+// given initial caffeine amount, great 'limit' amount of data points along that amount of caffeine's half-life curve
+function createCurve(caffeine) {
+    var data = [];
+    var limit = 48;
+    var y = 0;
+    var dataSeries = { type: "line" };
+    var dataPoints = [];
+    var t;
+    for (t = 0; t < limit; t += 1) {
+        y = caffeine * Math.pow(0.5, t/5.7)  // caffeine half-life equation
+        dataPoints.push({
+                        x: t,
+                        y: y
+                        });
+    }
+    dataSeries.dataPoints = dataPoints;
+    data.push(dataSeries);
+    return data;
 }
