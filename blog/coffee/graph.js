6bdd9206f46570a75b4452d2605f938f1aa9a575
Ben Clark <benclark@udel.edu>
2019-08-10 20:09:55 -0400

    eslint and jslint update

--- blog/coffee/graph.js
+++ blog/coffee/graph.js
@@ -1,4 +1,4 @@
-var dataSeries = { type: "line" };
+var dataSeries = { type: 'line' };
 var limit = 48; // amount of hours to graph after original dose
 var dataPoints = Array(limit).fill(0);
 var additionalDose = [];
@@ -7,41 +7,42 @@ var additionalTime = [];
 
 window.onload = function () {
     var caffeine = 180; // default caffeine amount
+    var t_2mg = 8.22336173*Math.log(caffeine) - 5.7;   // half-life formula in terms of caffeine
     var data = createCurve(caffeine, 0);
-    var two_mg;
-    
-    var chart = new CanvasJS.Chart("chartContainer",
+
+    var chart = new CanvasJS.Chart('chartContainer',
     {
         title: {
-            text: "Caffeine Half-life"
+            text: 'Caffeine Half-life'
         },
         axisX: {
-            title: "Hours after dose"
+            title: 'Hours after dose'
         },
         axisY: {
-            title: "Caffeine (mg)",
+            title: 'Caffeine (mg)'
         },
         data: data
     });
-    
+
     chart.render(); // render default chart
-    
+
     // when user changes initial caffeine amount
     var renderButton = document.getElementById('renderButton');
     renderButton.addEventListener('click', function () {
         caffeine = Number(document.getElementById('caffeine').value);
-        
+        t_2mg = 8.22336173*Math.log(caffeine) - 5.7;
+
         chart.options.data = createCurve(caffeine, 0);
-        
-        t_2mg = 8.22336173*Math.log(caffeine) - 5.7;   // half-life formula in terms of caffeine
-        document.getElementById('two_mg').innerHTML = t_2mg.toFixed(2) + " hours (" + Math.floor(t_2mg/24) + " days " + Math.round(t_2mg % 24) + " hours)";
-        
+
+        document.getElementById('two_mg').innerHTML = t_2mg.toFixed(2) + ' hours (' + Math.floor(t_2mg/24) + ' days ' +
+                                                      Math.round(t_2mg % 24) + ' hours)';
+
         chart.render();
-        
+
         additionalDose = [];
         additionalTime = [];
     });
-    
+
     // when user adds additional dose
     var newCaffeineButton = document.getElementById('newCaffeineButton');
     newCaffeineButton.addEventListener('click', function() {
@@ -62,8 +63,9 @@ window.onload = function () {
             }
             t_2mg = 5.7*Math.log2(sum) - 5.7;
         }
-        document.getElementById('two_mg').innerHTML = t_2mg.toFixed(2) + " hours (" + Math.floor(t_2mg/24) + " days " + Math.round(t_2mg % 24) + " hours)";
-        
+        document.getElementById('two_mg').innerHTML = t_2mg.toFixed(2) + ' hours (' + Math.floor(t_2mg/24) + ' days ' +
+                                                      Math.round(t_2mg % 24) + ' hours)';
+
         createCurve(newDose, newTime);
         chart.render();
     });
@@ -74,24 +76,25 @@ window.onload = function () {
 function createCurve(caffeine, time) {
     var data = [];
     var y = 0;
-    
+    var step;
+
     if (time == 0) {    // initial caffeine dose
-        var step = 0;
+        step = 0;
         for ( ; step < limit; step += 1) {
             y = caffeine * Math.pow(0.5, step/5.7)  // caffeine half-life equation
             dataPoints[step] = {x: step, y: y};
         }
     } else {    // additional dose
         var j;
-        var step = Math.ceil(time);
+        step = Math.ceil(time);
         var cTime = Math.ceil(time);
         var decTime = time % 1;
         for (j = 0; j < limit - cTime; j += 1) {
-            y = dataPoints[cTime + j]["y"] + caffeine * Math.pow(0.5, (j + decTime)/5.7)  // caffeine half-life equation
+            y = dataPoints[cTime + j]['y'] + caffeine * Math.pow(0.5, (j + decTime)/5.7)  // caffeine half-life equation
             dataPoints[cTime + j] = {x: cTime + j, y: y};
         }
     }
-    
+
     dataSeries.dataPoints = dataPoints;
     data.push(dataSeries);
     return data;
